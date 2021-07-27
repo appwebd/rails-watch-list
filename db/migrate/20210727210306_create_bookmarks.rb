df2b52e7922d4f8cc0241f2ba03992c870119ae5
Patricio Rojas Ortiz <patricio-rojaso@outlook.com>
2021-07-27 17:05:12 -0400

    Was generated model bookmark

---
+++ db/migrate/20210727210306_create_bookmarks.rb
@@ -0,0 +1,11 @@
+class CreateBookmarks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :bookmarks do |t|
+      t.integer :id
+      t.string  :comment
+      t.integer :movie_id
+      t.integer :list_id
+      t.timestamps
+    end
+  end
+end

782481162f9e967a8d5edc13869aa7bb0b4bfae6
Patricio Rojas Ortiz <patricio-rojaso@outlook.com>
2021-07-28 17:59:46 -0400

    was solve list index/create/view/delete

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,10 +1,41 @@
 class ListsController < ApplicationController
-  def index
-  end
+  before_action :set_find_list, only: %i[show edit update destroy]
 
-  def show
+  def index
+    @lists = List.all
   end
 
   def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(params_list)
+    @list.save
+    redirect_to lists_path(@list.id)
+  end
+
+  def edit; end
+
+  def update
+    @list.update(params_list)
+    redirect_to lists_path(@list.id)
+  end
+
+  def show; end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_path(@lists)
+  end
+
+  private
+
+  def params_list
+    params.require(:list).permit(:name)
+  end
+
+  def set_find_list
+    @list = List.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'lists', to: 'lists#index'
-  get 'lists/index', to: 'lists#index'
-  get 'lists/show'
-  get 'lists/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # view
+  #
+
+
+  resources :lists
+
+
 
 end
